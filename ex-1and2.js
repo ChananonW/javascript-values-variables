0dd6148aead8859eb99cce92b5c766584dcb17b7
FrankCW <wongasa999@gmail.com>
2024-09-05 20:41:46 +0700

    ex1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,10 @@
-//Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let surname = "Anderson";
+let addrees = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+let restaurantName = "Chef's Table by Chef James";
+let totalPrice = 1500;
+let totalRestaurant = 1500;
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+addrees = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+totalRestaurant = 2500;
